78a38523907b6da0f5a719bb12503bbea1ded690
Saher <saher.suga@gmail.com>
2020-09-01 22:38:12 -0500

    jumping

--- FirstGame.py
+++ FirstGame.py
@@ -3,10 +3,16 @@ pygame.init()
 win = pygame.display.set_mode((500,500))
 pygame.display.set_caption("First Game")
 
+##for jumping 
+isjump=False
+jumpcount=10
+##coordinates
 x=50
 y=50
-wedith=40
+##size of the box
+width=40
 height=60
+##speed 
 vel=20
 
 run = True
@@ -16,16 +22,32 @@ while run:
         if event.type==pygame.QUIT:
             run=False
     keys=pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT]  and x > vel :
         x-= vel
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < 500 - width - vel :
         x+=vel
-    if keys[pygame.K_UP]:
-        y-=vel
-    if keys[pygame.K_DOWN]:
-        y+=vel
+    if not(isjump):
+        if keys[pygame.K_UP]    and y > vel:
+            y-=vel
+        if keys[pygame.K_DOWN]  and y < 500 - height - vel:
+            y+=vel
+        if keys[pygame.K_SPACE]:
+            isjump = True
+    else:
+        if jumpcount>=-10:
+            neg=1
+            if jumpcount<0:
+                neg=-1
+            y-=(jumpcount**2)*0.5 *neg
+            jumpcount-=1
+        else:
+            isjump=False
+            jumpcount=10
+            
+
+
     win.fill((0,0,0))
-    pygame.draw.rect(win, (255,0,0), (x, y, wedith, height))
+    pygame.draw.rect(win, (255,0,0), (x, y, width, height))
     pygame.display.update()
     
 pygame.quit()
